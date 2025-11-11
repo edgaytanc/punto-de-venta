1f5167789067bdc715677cc4bead27c8a26a646f
Edwin5886 <epatzanp3@miumg.edu.gt>
2025-11-11 13:08:18 -0600

    fix: actualizar texto de bienvenida a Comercial Abi

--- backend/Models/Rol.cs
+++ backend/Models/Rol.cs
@@ -2,10 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MiApi.Models
 {
-    // Heredamos de IdentityRole<int> para usar un 'int' como llave primaria
+    
     public class Rol : IdentityRole<int>
     {
-        // IdentityRole ya tiene Id y Name. Podemos añadir más detalles.
+      
         public string DescripcionRol { get; set; }
     }
 }
